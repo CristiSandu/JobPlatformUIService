b360692e560406478847a85d5b97f44b1b59aeae
Ilie-Cristian SANDU <ilie_cristian.sandu@stud.acs.upb.ro>
2022-05-10 13:43:38 +0300

    Changes:  -  add authorizatin on backend  - add credentials in appsettings

---
+++ JobPlatformUIService/Authorization/AuthorizationExtensions.cs
@@ -0,0 +1,53 @@
+﻿
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace JobPlatformUIService.Authorization
+{
+    public static class AuthorizationExtensions
+    {
+        public static void AddAuth(this WebApplicationBuilder builder)
+        {
+          
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = builder.Configuration["IdentityServerAuthenticationOptions:Authority"]; ;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = builder.Configuration["IdentityServerAuthenticationOptions:Authority"],
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = builder.Configuration["IdentityServerAuthenticationOptions:Project_id"],
+                    ValidateLifetime = true
+                };
+            });
+
+            string[] AuthSchemes = { JwtBearerDefaults.AuthenticationScheme };
+            builder.Services.AddAuthorization(options =>
+            {
+                options.DefaultPolicy =
+                    new AuthorizationPolicyBuilder(AuthSchemes)
+                        .RequireAuthenticatedUser()
+                        .Build();
+            });
+        }
+
+        public static void UseAuthenticationAndAuthorization(this WebApplication app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
--- JobPlatformUIService/Program.cs
+++ JobPlatformUIService/Program.cs
@@ -1,7 +1,9 @@
+using JobPlatformUIService.Authorization;
 using JobPlatformUIService.Infrastructure.Data;
 using JobPlatformUIService.Infrastructure.Data.Firestore;
 using JobPlatformUIService.Infrastructure.Data.Firestore.Interfaces;
 using MediatR;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,14 +13,46 @@ builder.Services.Configure<FirestoreSettings>(builder.Configuration.GetSection("
 builder.Services.AddTransient<IFirestoreContext, FirestoreContext>();
 builder.Services.AddTransient(typeof(IFirestoreService<>), typeof(FirestoreService<>));
 
+builder.AddAuth();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Job Platform UI Service API", Version = "v1" });
+    var security = new Dictionary<string, IEnumerable<string>>
+    {
+        {"Bearer", System.Array.Empty<string>()},
+    };
+
+    c.AddSecurityDefinition("JWT", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.ApiKey,
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Description = "Enter the JWT in the following format 'Bearer AUTH_CODE'",
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "JWT"
+                }
+            },
+            System.Array.Empty<string>()
+        }
+    });
+
+});
 
 builder.Services.AddMediatR(typeof(Program));
-
 
 var app = builder.Build();
 
@@ -28,7 +62,12 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
+app.UseHttpLogging();
+
+app.MapControllers().RequireAuthorization();
+
 app.UseAuthorization();
+app.UseAuthenticationAndAuthorization();
 
 app.MapControllers();
 
